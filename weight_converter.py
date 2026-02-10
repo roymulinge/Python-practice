de1cf62ce71fa37dac0f0050db361989db17c77d
Roy <mutuaroymulinge@gmail.com>
2026-02-10 20:41:23 +0300

    reafactor: condition to store converted unit

--- weight_converter.py
+++ weight_converter.py
@@ -4,7 +4,9 @@ unit = input("Enter unit of measurement(K for kgs, L for lbs): ").upper()
 
 if unit == "K":
     weight = weight * 2.205
+    unit = "L"
 elif unit == "L":
     weight = weight / 2.205
+    unit = "K"
 else:
     print("Invalid unit!")
