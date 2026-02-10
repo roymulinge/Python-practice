855c40a8f1fb51f84dc82c73caf185a3964fd4eb
Roy <mutuaroymulinge@gmail.com>
2026-02-10 20:39:27 +0300

    feat: add looping control for unit checking and calculation

--- weight_converter.py
+++ weight_converter.py
@@ -2,3 +2,9 @@
 weight = float(input("Enter your weight: "))
 unit = input("Enter unit of measurement(K for kgs, L for lbs): ").upper()
 
+if unit == "K":
+    weight = weight * 2.205
+elif unit == "L":
+    weight = weight / 2.205
+else:
+    print("Invalid unit!")
