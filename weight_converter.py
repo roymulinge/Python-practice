534023af3f1a600cb855628920dbdf45df9fcb38
Roy <mutuaroymulinge@gmail.com>
2026-02-10 20:45:48 +0300

    feat: add looping control and loop exiting

--- weight_converter.py
+++ weight_converter.py
@@ -1,12 +1,19 @@
 #Weight converter
-weight = float(input("Enter your weight: "))
-unit = input("Enter unit of measurement(K for kgs, L for lbs): ").upper()
 
-if unit == "K":
-    weight = weight * 2.205
-    unit = "L"
-elif unit == "L":
-    weight = weight / 2.205
-    unit = "K"
-else:
-    print("Invalid unit!")
+while True:
+    quit = input("Press Q to quit program or enter to continue: ")
+    if quit == "Q":
+        break
+    weight = float(input("Enter your weight: "))
+    unit = input("Enter unit of measurement(K for kgs, L for lbs): ").upper()
+
+    if unit == "K":
+        weight = weight * 2.205
+        unit = "L"
+    elif unit == "L":
+        weight = weight / 2.205
+        unit = "K"
+    else:
+        print("Invalid unit!")
+
+    print(f"You weight is {weight} {unit}")
