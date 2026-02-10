3dd38e19be8883cbeb16ea5531310ea5dd47a4d0
Roy <mutuaroymulinge@gmail.com>
2026-02-10 20:53:26 +0300

    feat: add exception error handling

--- weight_converter.py
+++ weight_converter.py
@@ -1,20 +1,24 @@
 #Weight converter
 
 while True:
-    quit = input("Press Q to quit program or enter to continue: ").upper()
-    if quit == "Q":
-        print("Run app to convert weight!")
-        break
-    weight = float(input("Enter your weight: "))
-    unit = input("Enter unit of measurement(K for kgs, L for lbs): ").upper()
+    try:
+    
+        quit = input("Press Q to quit program or enter to continue: ").upper()
+        if quit == "Q":
+            print("Run app to convert weight!")
+            break
+        weight = float(input("Enter your weight: "))
+        unit = input("Enter unit of measurement(K for kgs, L for lbs): ").upper()
 
-    if unit == "K":
-        weight = weight * 2.205
-        unit = "L"
-    elif unit == "L":
-        weight = weight / 2.205
-        unit = "K"
-    else:
-        print("Invalid unit!")
+        if unit == "K":
+            weight = weight * 2.205
+            unit = "L"
+        elif unit == "L":
+            weight = weight / 2.205
+            unit = "K"
+        else:
+            print("Invalid unit!")
 
-    print(f"You weight is {weight} {unit}")
+        print(f"You weight is {weight} {unit}")
+    except ValueError:
+        print("Invalid weight! Try again")
