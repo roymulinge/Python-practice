c19a5a2bbf4cf837d15ac03f7ca8b04169652adb
Roy <mutuaroymulinge@gmail.com>
2026-02-10 20:37:01 +0300

    fix: correct unit input prompt

--- weight_converter.py
+++ weight_converter.py
@@ -1,4 +1,4 @@
 #Weight converter
 weight = float(input("Enter your weight: "))
-unit = input("Enter unit of measurement: ")
+unit = input("Enter unit of measurement(K for kgs, L for lbs): ").upper()
 
